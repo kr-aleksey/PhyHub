ffdf295f094c420e5afaed1c22ffa68bc0828a62
Aleksey Kravtsun <kravtsun.aleksey@gmail.com>
2023-08-25 13:22:36 +0300

    Celery

--- backend/PhyHub/settings.py
+++ backend/PhyHub/settings.py
@@ -134,3 +134,7 @@ MEDIA_URL = ''
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery settings
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
