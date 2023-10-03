c8f42f16e3a5058a5f71acb473066e185fb9fb77
a_kravtsun <a_kravtsun@polipak.org>
2023-10-03 11:53:56 +0300

    add html template

--- backend/PhyHub/settings.py
+++ backend/PhyHub/settings.py
@@ -132,6 +132,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'PhyHub/static'),
+]
+
 # DRF
 
 REST_FRAMEWORK = {
--- backend/sensors/views.py
+++ backend/sensors/views.py
@@ -1,4 +1,5 @@
-from django.db.models import BooleanField, Value
+from datetime import datetime
+
 from django.views.generic import DetailView
 
 from .filters import SensorReadingFilter
@@ -8,7 +9,7 @@ from .models import Sensor
 class SensorReadingView(DetailView):
     model = Sensor
     context_object_name = 'sensor'
-    template_name = 'sensors/sensor_readings.html'
+    template_name = 'sensors/sensor_readings_.html'
     filterset_class = SensorReadingFilter
 
     @staticmethod
@@ -16,6 +17,17 @@ class SensorReadingView(DetailView):
         data = (
             f'[new Date({int(r.measured_at.timestamp()) * 1000}), {r.value}]'
             for r in readings
+        )
+        s = ', '.join(data)
+        return f'[{s}]'
+
+    @staticmethod
+    def qs_to_timeline_data(intervals):
+        data = (
+            (f"['{i.status}', "
+             f"new Date({int(i.started_at.timestamp()) * 1000}), "
+             f"new Date({(int((i.finished_at or datetime.now()).timestamp()) * 1000)})]")
+            for i in intervals
         )
         s = ', '.join(data)
         return f'[{s}]'
@@ -29,4 +41,13 @@ class SensorReadingView(DetailView):
 
         data['readings_filter'] = readings_filter
         data['chart_data'] = self.qs_to_chart_data(readings_filter.qs)
+        f = readings_filter.qs[0]
+        l = readings_filter.qs[len(readings_filter.qs) - 1]
+        data['timeline_data'] = self.qs_to_timeline_data(
+            (self
+             .object
+             .working_intervals
+             .filter(finished_at__gt=f.measured_at, started_at__lte=l.measured_at))
+        )
+
         return data
